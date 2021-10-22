7006fc1e49732cb6c020741649224c19d3efe49d
vood <balitsky_igor@list.ru>
2021-10-22 20:22:18 +0300

    test

---
+++ src/test/java/ru/vood/lib/async/AsyncOperationsTest.kt
@@ -0,0 +1,28 @@
+package ru.vood.lib.async
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.util.function.BiConsumer
+
+internal class AsyncOperationsTest {
+
+    @Test
+    @DisplayName("Тест на асинхронность запуска без DSL")
+    fun testOnAsyncRun() {
+        val threads = mutableSetOf<String>()
+        val threadsErr = mutableSetOf<String>()
+        val workList = IntRange(1, 100).map { it.toString() }.toList()
+        AsyncOperations<String, Int, Map<Any, Any>>(
+            doOnFail = BiConsumer { v, ex -> threadsErr.add(Thread.currentThread().name) },
+            doOnSuccess = { t, r -> threads.add(Thread.currentThread().name) },
+            resultCombiner = { map -> mapOf() }
+        ).applyBatchOfValues(
+            workList.map { AsyncValue(it) },
+            work = { s: String -> s.toInt() }
+        )
+
+        Assertions.assertTrue(threads.size > 1)
+        Assertions.assertTrue(threadsErr.size == 0)
+    }
+}
